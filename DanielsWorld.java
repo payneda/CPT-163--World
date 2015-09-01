01553ba67ca1b539601ae12a1ee0dd9bf9a57b2b
Daniel Payne <payneda@my.wccc.edu>
2015-09-01 19:34:18 -0400

    Hello World
    n/a

---
+++ DanielsWorld.java
@@ -0,0 +1,31 @@
+
+
+
+
+
+package daniels.world;
+
+
+public class DanielsWorld {
+    public static void main(String[] args) {
+       
+        int twoTimesThree = 2*3;
+        final int ISZERO = 0;
+        int isTwo = 2;
+        int isThree = 3;
+        
+        System.out.println(twoTimesThree);
+        System.out.println(isTwo * isThree);
+        
+        // YEY COMMENTS
+        /*int variableName = 10; 
+        double anotherVariableName = 98.6;
+        int wasADouble = (int)anotherVariableName;
+        int dropThatDecimal = (int )9.8;
+        System.out.println("HI CLASS");
+        System.out.println(variableName);
+        System.out.println(anotherVariableName);
+        System.out.println(wasADouble);*/
+    }
+
+}
